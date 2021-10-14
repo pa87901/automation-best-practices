ffc1561685eaf206c37034788dd7dafa89a525c1
pa87901 <pa87901@users.noreply.github.com>
2021-10-14 10:32:04 -0700

    Update App.js for a new image.

--- testing-for-charity/my-react-app/src/App.js
+++ testing-for-charity/my-react-app/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import logo from './mia.jpg';
+// import logo from './mia.jpg';
+import europa from './220px-Europa-moon-with-margins.jpg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={europa} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
